1f22b61ba3532e754eacf0d6905ef144f3fbe86c
Kade <keithkade@gmail.com>
2019-02-25 20:44:25 -0600

    comment

--- scripts/run_bert_squad.py
+++ scripts/run_bert_squad.py
@@ -1,5 +1,10 @@
-import os
+"""
+https://github.com/huggingface/pytorch-pretrained-BERT#squad
 
+Runs a squad eval
+"""
+
+import os
 
 command = 'python ./BERT/examples/run_squad.py \
 --bert_model bert-base-uncased \
