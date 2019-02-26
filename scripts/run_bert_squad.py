41a6147496010138ab4d262d299e58b02e4784f4
Kade <keithkade@gmail.com>
2019-02-25 20:43:05 -0600

    formatting

--- scripts/run_bert_squad.py
+++ scripts/run_bert_squad.py
@@ -1,5 +1,10 @@
 import os
 
 
-command = 'python ./BERT/examples/run_squad.py --bert_model bert-base-uncased --output_dir tmp --do_predict --predict_file ./data/dev-v1.1.json'
+command = 'python ./BERT/examples/run_squad.py \
+--bert_model bert-base-uncased \
+--output_dir tmp \
+--do_predict \
+--predict_file ./data/dev-v1.1.json'
+
 os.system(command)
