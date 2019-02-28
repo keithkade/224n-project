7226b7c2eb1928b3f00db2b16c39795a0aeafe2b
Kade <keithkade@gmail.com>
2019-02-27 22:49:29 -0600

    change file to run bert predictions on

--- scripts/run_bert_squad.py
+++ scripts/run_bert_squad.py
@@ -10,6 +10,6 @@ command = 'python ./BERT/examples/run_squad.py \
 --bert_model bert-base-uncased \
 --output_dir tmp \
 --do_predict \
---predict_file ./data/dev-v1.1.json'
+--predict_file ./squad-starter/data/dev-v2.0.json'
 
 os.system(command)
